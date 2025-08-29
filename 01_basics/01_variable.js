c7a7167ea21c72f1ac50228f43fc9d28863f8f7a
singhalsujal1234 <singhalsujal1234@gmail.com>
2025-08-29 07:07:30 +0000

    config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail= "sujal@google.com"
+var accountcity = "jaipur"
+// accountId =2
+let accountstate;
+
+console.log (accountId);
+
+
+accountEmail="hc@hc.com"
+accountpassword ="212212"
+accountcity ="delhi"
+
+console.table([accountId,accountEmail,accountpassword,accountcity])

e1a1ebae4269b26526b8129c6191fd7da41b0236
singhalsujal1234 <singhalsujal1234@gmail.com>
2025-08-29 07:43:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all code as newer version 
+// alert(3+3)we r using nodejs nt browser
+console.log(3+3)
+
+console.log("sujal")
+
+let name ="sujal"
+let age = 18
+let isloggedIn =false
+let state;
+/*
+
+number = 2 to power 53 
+bigint 
+string =""
+boolean =true /false
+null = standalone value 
+undefined =
+symbol =unique 
+ 
+object'
+ 
+*/
+console.log(typeof "sujal")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)
+

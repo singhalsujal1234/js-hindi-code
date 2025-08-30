963b6a664468cb726d64badc45fb1d9d83acc02c
singhalsujal1234 <singhalsujal1234@gmail.com>
2025-08-30 13:11:23 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null > 0);
+// console.log(null==0);//eqaulity opertor null hi rakhta h 
+// console.log(null>=0);//or comparison opertaor null ko 0 me change krr deta h 
+
+
+// console.log(undefined > 0);
+// console.log(undefined ==0);//eqaulity opertor null hi rakhta h 
+// console.log(undefined>=0);
+
+
+console.log("2"===2)

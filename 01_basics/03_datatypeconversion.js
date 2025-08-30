3aad5afdc11d2166beeca026e04b386c400320b1
singhalsujal1234 <singhalsujal1234@gmail.com>
2025-08-30 12:18:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,27 @@
+// let score ="33"
+
+// console.log (typeof score );
+// console.log (typeof (score));
+// let valueinnumber =Number(score)
+// console.log (typeof valueinnumber);
+// console.log(valueinnumber);
+
+
+// "33"-----33
+// "33abc"------NaN
+// true ---1;false----0
+
+// let isloggedin =""
+// let booleanisloggedin =Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+
+// 1---true ; 0---false
+// ""---false
+// "sujal"---true
+
+
+let somenumber =33
+let string =String(somenumber)
+console.log(string);
+console.log(typeof string)

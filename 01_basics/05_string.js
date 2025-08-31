351145afe833044e0af9be0c403dc2d652f28b60
singhalsujal1234 <singhalsujal1234@gmail.com>
2025-08-31 13:27:48 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName =  '  hitesh-hc-com'
+console.log(big(3));
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);//prototype pta chlta h 
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(gameName.substring(0,4));
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(gameName.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split(''));
